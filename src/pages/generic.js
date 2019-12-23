ed359640d6568a7b53eba4fd28f63a291cac1da8
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2019-12-23 17:31:41 +0200

    New dates

--- src/pages/generic.js
+++ src/pages/generic.js
@@ -25,7 +25,36 @@ const SecondPage = () => (
           The main ceremony will be held on Saturday 18 January 2020. It will be an all-night, open-air ceremony around Tatewari the grandfather fire.
 On Friday the 17th of January there will be a cultural exchange evening, everyone attending ceremony is welcome. Saturday morning early there will be opportunity to see traditional soap cooking on an open fire. In preparation for Saturday night there will be a sweat lodge. On Sunday we will close with a Cacao ceremony led by Cacao Kuchina Matthew Bloemetje, healings, prayers and offerings.
           </p>
-          <h4></h4>
+          <h5>COST</h5>
+          <p>The cost of the weekend is R2 900 per person this includes:</p>
+<p>Tea and two vegetarian meals (Saturday evening, Sunday morning)</p>
+<p>Cultural experience</p>
+<p>Camping</p>
+<p>Cacao ceremony led by Kuchina Matthew</p>
+<p>Sweat lodge led by Don Eugenio</p>
+<p>All night Hikuri ceremony</p>
+<p>Medicine gathered on an arduous pilgrimage and handled in the most sacred of ways.</p>
+<p>Accomodation is only needed for the Friday night cultural evening-Saturday night is an all night ceremony. You are welcome to bring a tent and camp on a first-come-first basis, amenities are very rustic and basic.
+There is also a wonderful bed and breakfast based on self sustainability on the property next to the ceremonial space. Bookings can be done at <a href="​https://sunplace.co.za/">https://sunplace.co.za/</a></p>
+          <h4>21 Jan 2020 Eagle's Valley</h4>
+          <br/>
+          <br/>
+          <h4>24 Jan - 26 Jan 2020 KwaZulu-Natal</h4>
+          <p>The ceremony will take place on a private residence in Nkanyezini, Midlands. Close to the Lion Park
+          The main ceremony will be held on Saturday 24th January 2020. It will be an all-night, open-air ceremony around Tatewari the grandfather fire. On Sunday we will either go down to the river or to the mountain for prayers and offerings.</p>
+          <h5>COST</h5>
+          <p>
+The cost of the weekend is R2 830 per person this includes:</p>
+<p>Tea and two vegetarian meals (Saturday evening, Sunday morning)</p>
+<p>Camping</p>
+<p>All night Hikuri ceremony</p>
+<p>Medicine gathered on an arduous pilgrimage and handled in the most sacred of ways.</p>
+<p>Saturday night is an all-night ceremony. You are welcome to bring a tent and camp on a first-come-first basis, amenities are very rustic and basic.</p>
+<h4>31 Jan - 2 Feb 2020 Western Cape</h4>
+<p>The ceremony will take place at Sacred Ibis in Hout Bay, Cape Town
+The main ceremony will be held on Saturday 1 February 2020. It will be an all-night, open-air ceremony around Tatewari the grandfather fire. On Sunday we will be a ceremony for prayers and offerings.</p>
+<h4>7 Feb - 9 Feb 2020 Namibia</h4>
+<p>Please contact to request more details about this ceremony. The main ceremony will be held on Saturday 8th February 2020. It will be an all-night, open-air ceremony around Tatewari the grandfather fire. On Sunday we will be a ceremony for prayers and offerings.</p>
         </div>
       </section>
     </div>
